70408ddf5d070a732a02dc994fe335a7672fcf56
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-04-16 01:24:36 +0100

    Styling

--- store/src/components/NavbarComp.js
+++ store/src/components/NavbarComp.js
@@ -1,13 +1,25 @@
 import React, { useContext, useState } from 'react'
-import { Button, Navbar, Modal } from 'react-bootstrap'
+import { Button, Navbar, Modal, Dropdown, } from 'react-bootstrap'
 import { CartContext } from '../CartContext'
 import CartProduct from './CartProduct'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { BiLogOut} from 'react-icons/bi'
+import {CgProfile}from 'react-icons/cg'
 
 const NavbarComp = () => {
+  const isLoggedIn = true;
   const cart = useContext(CartContext)
   const [show, setShow] = useState(false)
+  const [showProfile, setShowProfile] = useState(false);
+
+  
+
+
+
+  const handleProfileToggle = () => {
+    setShowProfile(!showProfile);
+  };
 
   const handleClose = () => {
     setShow(false)
@@ -51,15 +63,37 @@ const NavbarComp = () => {
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <div className='px-3'>
-          <Button variant="outline-secondary" className="rounded-pill mr-2 mx-2">
-            Sign Up
-          </Button>
-          <Button variant="outline-secondary" className="rounded-pill" onClick={handleShow}>
-            <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />
-             {productCount} Items
-          </Button>
+        <div className="d-flex align-items-center px-3 ">
+            {isLoggedIn ? (
+              <Dropdown className='px-3'>
+                <Dropdown.Toggle variant="link" id="dropdown-basic" className="p-0">
+                  <img
+                    class="rounded-circle"
+                    height="30"
+                    className="d-inline-block align-top mx-2 rounded-circle "
+                    alt="avatar1"
+                    src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp"
+                    onClick={handleProfileToggle}
+                  />
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu show={showProfile}>
+                  <Dropdown.Item href="#/action-1"><CgProfile />Profile</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2"><BiLogOut className='mr-1'/>Logout</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            ) : (
+              <Button variant="outline-secondary" className="rounded-pill mr-2 mx-2">
+                Sign Up
+              </Button>
+            )}
+
+            <Button variant="outline-secondary" className="rounded-pill " onClick={handleShow}>
+              <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />
+              {productCount} Items
+            </Button>
           </div>
+          
         </Navbar.Collapse>
       </Navbar>
       <Modal show={show} onHide={handleClose}>
