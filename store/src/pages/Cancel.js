8533d577b26f850304e909dc5370fe7e2e207cb0
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-04-16 18:09:40 +0100

    fixed

--- store/src/pages/Cancel.js
+++ store/src/pages/Cancel.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 const Cancel = () => {
   return (
     <Container className="my-5">
       <Row>
         <Col>
+          <div className="d-flex justify-content-center">
+            <FontAwesomeIcon icon={faTimesCircle} size="6x" color="red" />
+          </div>
           <h1 className="text-center mb-4">Payment Cancelled</h1>
           <p className="text-center">We're sorry to see you go. If you'd like to try again, simply click the button below to go back to the home page.</p>
           <div className="d-flex justify-content-center">
-            <Button variant="primary" size="lg" href="/" className="mt-4">Go Back to Checkout</Button>
+            <Button variant="success" size="lg" href="/" className="mt-4">Go Back to store</Button>
           </div>
         </Col>
       </Row>
--- store/src/productStore.js
+++ store/src/productStore.js
@@ -5,40 +5,40 @@
 
 const productsArray = [
     {
-        id:"price_1Mx9jIEXYQfbBZFa9YQ6aWIM",
+        id:"price_1MxYrrEXYQfbBZFacmhm7QfO",
         title:"T-shirt",
         img:"T-shirt.jpg",
         price :45.99
     },
     {
-        id:"price_1Mx9nYEXYQfbBZFaxzwbXYPs",
+        id:"price_1MxYsTEXYQfbBZFawbRuVC62",
         title:"Jacket",
         img: "jacket.jpg",
         price : 119.99
     },
     {
-        id:"price_1Mx9qhEXYQfbBZFaMd2T2Yg2",
+        id:"price_1MxYu7EXYQfbBZFaAhkoiFau",
         title:"Black Shirt",
         img:'blackshirt.jpg',
         price : 11.99
     },
     {
-        id:"jeans",
+        id:"price_1MxYvrEXYQfbBZFanKrDWaFd",
         title:"Jeans",
         img:"jeans.jpg",
         price : 44.99
     },
     {
-        id:"dfsfd",
+        id:"price_1MxYwrEXYQfbBZFa6d3gqbf6",
         title:"Sweater",
         img:"sweater.jpg",
         price : 25.99
     },
     {
-        id:"price_1Mx9qhEXYQfbBZFaMd2T2Yg2",
+        id:"price_1MxYxnEXYQfbBZFa8h3ClwDr",
         title:"SweatShirt",
         img:"sweatshirt.jpg",
-        price : 11.99
+        price : 15.99
     }
 ]
 
